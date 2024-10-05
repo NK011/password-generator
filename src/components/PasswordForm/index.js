5e3df52f7ec34f6844b1a1cc79912e3f89332640
NitishKumar011 <nitish.kumar@geeksforgeeks.org>
2024-10-05 23:59:54 +0530

    chabges

--- src/components/PasswordForm/index.js
+++ src/components/PasswordForm/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/components/PasswordGenerator.module.css";
 function FormField({ type, name, label, onChange }) {
     return (
@@ -10,38 +10,85 @@ function FormField({ type, name, label, onChange }) {
 }
 
 function PasswordForm({ setGeneratedPassword = () => {} }) {
+    const defaultFieldValues = {
+        passwordLength: 8,
+        uppercase: false,
+        lowercase: false,
+        numbers: false,
+        specialChar: false,
+    };
+
+    const [formState, setFormState] = useState(defaultFieldValues);
     const handleInputChange = (e) => {
-        console.log(e.target.checked);
+        const values = { ...formState };
+        const { name, checked } = e?.target;
+        values[name] = checked;
+        setFormState(values);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let tempPassword = ""
+
+        if(formState.uppercase) {
+            tempPassword += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        }
+        if(formState.lowercase) {
+            tempPassword += "abcdefghijklmnopqrstuvwxyz"
+        }
+        if(formState.numbers) {
+            tempPassword += "1234567890"
+        }
+        if(formState.numbers)
     };
     return (
-        <form className={styles.form__container}>
+        <form className={styles.form__container} onSubmit={handleSubmit}>
+            <div className={styles.form__row}>
+                <input
+                    type="range"
+                    name="passwordLength"
+                    min="8"
+                    max="20"
+                    className={styles.form__rangeField}
+                    onChange={handleInputChange}
+                    value={formState["passwordLength"]}
+                />
+            </div>
             <div className={styles.form__row}>
                 <FormField
                     type="checkbox"
                     name="uppercase"
                     label="Include Uppercase"
                     onChange={handleInputChange}
+                    value={formState["uppercase"]}
                 />
                 <FormField
                     type="checkbox"
                     name="lowercase"
                     label="Include Lowercase"
                     onChange={handleInputChange}
+                    value={formState["lowercase"]}
                 />
             </div>
             <div className={styles.form__row}>
                 <FormField
                     type="checkbox"
-                    name="specialChar"
+                    name="numbers"
                     label="Include Numbers"
                     onChange={handleInputChange}
+                    value={formState["numbers"]}
                 />
                 <FormField
                     type="checkbox"
-                    name="lowercase"
+                    name="specialChar"
                     label="Include Spcial Characters"
                     onChange={handleInputChange}
+                    value={formState["specialChar"]}
                 />
+            </div>
+            <div className={styles.form__row}>
+                <FormField type="submit" name="submit" />
             </div>
         </form>
     );
